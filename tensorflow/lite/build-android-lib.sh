ed4445a87163ea1fcc7d868c2f86c3ff09106844
江战 <397136899@qq.com>
2024-05-30 18:25:38 +0800

    update script

--- tensorflow/lite/build-android-lib.sh
+++ tensorflow/lite/build-android-lib.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 ndk_path=/Users/admin/Library/Android/sdk/ndk/25.2.9519653
 set -x
-cmake  -Bbuild-android-lib-armeabi-v7a \
-    -S . \
-    -DCMAKE_INSTALL_PREFIX=install/android-armeabi-v7a \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DTFLITE_ENABLE_GPU=ON \
-    -DCMAKE_TOOLCHAIN_FILE="$ndk_path/build/cmake/android.toolchain.cmake" \
-    -DANDROID_PLATFORM=android-21 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DANDROID_ABI=armeabi-v7a \
-    -G Ninja
 
-cmake  -Bbuild-android-lib-arm64-v8a \
+for arch in armeabi-v7a arm64-v8a x86 x86_64
+do
+cmake  -Bbuild-android-lib-$arch \
     -S . \
-    -DCMAKE_INSTALL_PREFIX=install/android-arm64-v8a \
+    -DCMAKE_INSTALL_PREFIX=install/android-$arch \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DTFLITE_ENABLE_GPU=ON \
     -DCMAKE_TOOLCHAIN_FILE="$ndk_path/build/cmake/android.toolchain.cmake" \
     -DANDROID_PLATFORM=android-21 \
     -DCMAKE_BUILD_TYPE=Release \
-    -DANDROID_ABI=arm64-v8a \
+    -DANDROID_ABI=$arch \
     -G Ninja
+done
+
--- tensorflow/lite/copy-libs.sh
+++ tensorflow/lite/copy-libs.sh
@@ -2,21 +2,19 @@
 
 dst=~/code/blockpuzzle_cocos2dx/external
 
-build_dir=build-android-lib-armeabi-v7a
-dst_dir=$dst/android/armeabi-v7a/tf/
-#ninja -C $build_dir
+for arch in armeabi-v7a arm64-v8a x86
+do
+build_dir=build-android-lib-$arch
+dst_dir=$dst/android/$arch/tf/
+ninja -C $build_dir
 cp -v $build_dir/libtensorflow-lite.a $dst_dir
 cp -v $build_dir/_deps/xnnpack-build/libXNNPACK.a $dst_dir
 cp -v $build_dir/_deps/farmhash-build/libfarmhash.a $dst_dir
 cp -v $build_dir/_deps/cpuinfo-build/libcpuinfo.a $dst_dir
+cp -v install/android-$arch/lib/*.a $dst/android/$arch/tf/
+llvm-strip --strip-debug --strip-unneeded $dst/android/$arch/tf/*.a
+done
 
-build_dir=build-android-lib-arm64-v8a
-dst_dir=$dst/android/arm64-v8a/tf/
-#ninja -C $build_dir
-cp -v $build_dir/libtensorflow-lite.a $dst_dir
-cp -v $build_dir/_deps/xnnpack-build/libXNNPACK.a $dst_dir
-cp -v $build_dir/_deps/farmhash-build/libfarmhash.a $dst_dir
-cp -v $build_dir/_deps/cpuinfo-build/libcpuinfo.a $dst_dir
 
 build_dir=build-ios-lib
 dst_dir=$dst/ios/libs/tf
@@ -29,9 +27,3 @@ cp -v $build_dir/_deps/cpuinfo-build/libcpuinfo.a $dst_dir
 echo "copy ios libs"
 cp -v install/ios/lib/*.a  $dst/ios/libs/tf/
 llvm-strip --strip-debug $dst/ios/libs/tf/*.a
-echo "copy android arm64"
-cp -v install/android-arm64-v8a/lib/*.a $dst/android/arm64-v8a/tf/
-llvm-strip --strip-debug $dst/android/arm64-v8a/tf/*.a
-echo "copy android armeabi"
-cp -v install/android-armeabi-v7a/lib/*.a $dst/android/armeabi-v7a/tf/
-llvm-strip --strip-debug $dst/android/armeabi-v7a/tf/*.a
